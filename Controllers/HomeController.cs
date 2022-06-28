e4e635dac19ff953c49f9ad19511cfc40f2e43b3
BlackBrainFactory <48590849+BlackBrainFacotry@users.noreply.github.com>
2022-06-28 10:26:16 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using CurrencyCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace CurrencyCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        protected Root[] data;
+       
+        public HomeController(ILogger<HomeController> logger)
+        {            
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            GetDataFromApi();        
+            return View();
+        }
+
+    
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+      
+        public ActionResult  GetDataFromApi()
+        {           
+            var webClient = new WebClient();
+            var json = webClient.DownloadString(@"http://api.nbp.pl/api/exchangerates/tables/a/?format=json");
+            Models.Root[] objJson = JsonConvert.DeserializeObject<Models.Root[]>(json); 
+            return View(objJson);  
+        }
+    }
+}
---
+++ Models/JsonClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyCalculator.Models
+{
+    public class Rate
+    {
+        public string currency { get; set; }
+        public string code { get; set; }
+        public double mid { get; set; }
+    }
+
+    public class Root
+    {
+        public string table { get; set; }
+        public string no { get; set; }
+        public string effectiveDate { get; set; }
+        public List<Rate> rates { get; set; }
+    }
+
+}
